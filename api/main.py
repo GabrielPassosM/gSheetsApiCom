724a5766813f8ce9d7c839979a3a5197e03140e7
gabmartins <gabrielpassosmartins@gmail.com>
2025-02-26 16:02:43 -0300

    add rate limit to endpoint

--- api/main.py
+++ api/main.py
@@ -2,11 +2,14 @@ import base64
 import os
 from datetime import datetime, timezone
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import gspread
 from google.oauth2.service_account import Credentials
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 
 
 load_dotenv()
@@ -16,6 +19,10 @@ app = FastAPI(
     title="My communicator for the google sheets API",
     version="1.0.0"
 )
+
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
@@ -48,7 +55,9 @@ def read_root():
 
 
 @app.post("/add-row/{sheet_id}/{add_timestamp}", status_code=200)
+@limiter.limit("10/minute")
 def add_row(
+    request: Request,  # DONT remove - used by the limiter
     sheet_id: str,
     add_timestamp: bool,
     row_info: dict,
